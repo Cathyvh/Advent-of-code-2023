278f1d5c0757ca15cab6bfd21a42f97bf5e0c990
Cathyvh <53515655+Cathyvh@users.noreply.github.com>
2023-11-30 18:20:44 +0100

    add a little readme, remove logs and add gitignore

---
+++ README.md
@@ -0,0 +1,3 @@
+🎄 Advent of Code 2023 🎄
+
+Run yarn day # to see results of specific day, or yarn day to see the result of all days entered.
--- src/dayScript.ts
+++ src/dayScript.ts
@@ -2,9 +2,6 @@ import { logDayResults } from './app'
 
 const args = process.argv.slice(2)
 if (!args[0]) {
-  console.log(logDayResults())
 } else {
   const day = parseInt(args[0], 10)
-
-  console.log(logDayResults(day))
 }
